1bb49d8051e5ce7be1d3c74b7f4f388b6f98537b
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-11-20 21:32:46 +0100

    net: switchable Knox net implementation
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -149,9 +149,11 @@
 #include <net/busy_poll.h>
 
 /* START_OF_KNOX_NPA */
+#ifdef CONFIG_KNOX_NCM
 #include <linux/sched.h>
 #include <linux/pid.h>
 #include <net/ncm.h>
+#endif
 /* END_OF_KNOX_NPA */
 
 static DEFINE_MUTEX(proto_list_mutex);
@@ -672,6 +674,7 @@ out:
 }
 
 /* START_OF_KNOX_NPA */
+#ifdef CONFIG_KNOX_NCM
 /** The function sets the domain name associated with the socket. **/
 static int sock_set_domain_name(struct sock *sk, char __user *optval,
                 int optlen)
@@ -758,7 +761,7 @@ static int sock_set_dns_pid(struct sock *sk, char __user *optval, int optlen)
 out:
 	return ret;
 }
-
+#endif
 /* END_OF_KNOX_NPA */
 
 static inline void sock_valbool_flag(struct sock *sk, int bit, int valbool)
@@ -810,12 +813,14 @@ int sock_setsockopt(struct socket *sock, int level, int optname,
 		return sock_setbindtodevice(sk, optval, optlen);
 
     /* START_OF_KNOX_NPA */
+#ifdef CONFIG_KNOX_NCM
     if (optname == SO_SET_DOMAIN_NAME)
         return sock_set_domain_name(sk, optval, optlen);
     if (optname == SO_SET_DNS_UID)
 	return sock_set_dns_uid(sk, optval, optlen);
 	if (optname == SO_SET_DNS_PID)
 		return sock_set_dns_pid(sk, optval, optlen);
+#endif
     /* END_OF_KNOX_NPA */
 
 	if (optlen < sizeof(int))
@@ -1569,6 +1574,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 	struct sock *sk;
 
 	/* START_OF_KNOX_NPA */
+#ifdef CONFIG_KNOX_NCM
 	struct pid *pid_struct = NULL;
 	struct task_struct *task = NULL;
 	int process_returnValue = -1;
@@ -1577,12 +1583,14 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 	struct task_struct *parent_task = NULL;
 	int parent_returnValue = -1;
 	char full_parent_process_name[PROCESS_NAME_LEN_NAP] = {0};
+#endif
 	/* END_OF_KNOX_NPA */
 
 	sk = sk_prot_alloc(prot, priority | __GFP_ZERO, family);
 	if (sk) {
 		sk->sk_family = family;
 		/* START_OF_KNOX_NPA */
+#ifdef CONFIG_KNOX_NCM
 		/* assign values to members of sock structure when npa flag is present */
 		sk->knox_uid = current->cred->uid.val;
 		sk->knox_pid = current->tgid;
@@ -1624,6 +1632,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 				}
 			}
 		}
+#endif
 		/* END_OF_KNOX_NPA */
 		/*
 		 * See comment in struct sock definition to understand
