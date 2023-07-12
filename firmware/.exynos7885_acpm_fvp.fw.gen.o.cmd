644883232492def07a7044dcd3de12037cb41717
DeadPool-4422 <dynamicplayer444@gmail.com>
2023-07-12 20:23:05 +0530

    Fixing Firmware recursive Inclusion

---
+++ firmware/.exynos7885_acpm_fvp.fw.gen.o.cmd
@@ -0,0 +1,9 @@
+cmd_firmware/exynos7885_acpm_fvp.fw.gen.o := /home/dazai/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc -Wp,-MD,firmware/.exynos7885_acpm_fvp.fw.gen.o.d -nostdinc -isystem /home/dazai/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/../lib/gcc/aarch64-linux-gnu/4.9.4/include -I./arch/arm64/include -Iarch/arm64/include/generated/uapi -Iarch/arm64/include/generated  -Iinclude -I./arch/arm64/include/uapi -Iarch/arm64/include/generated/uapi -I./include/uapi -Iinclude/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian   -D__ASSEMBLY__ -fno-PIE -DCC_HAVE_ASM_GOTO           -c -o firmware/exynos7885_acpm_fvp.fw.gen.o firmware/exynos7885_acpm_fvp.fw.gen.S
+
+source_firmware/exynos7885_acpm_fvp.fw.gen.o := firmware/exynos7885_acpm_fvp.fw.gen.S
+
+deps_firmware/exynos7885_acpm_fvp.fw.gen.o := \
+
+firmware/exynos7885_acpm_fvp.fw.gen.o: $(deps_firmware/exynos7885_acpm_fvp.fw.gen.o)
+
+$(deps_firmware/exynos7885_acpm_fvp.fw.gen.o):
