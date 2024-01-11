6d923b8a29d5f6c8a1ba8933150d92df0cfac17d
DeadPool-4422 <123509971+DeadPool-4422@users.noreply.github.com>
2024-01-11 16:50:49 +0530

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 export ARCH=arm64
 export CROSS_COMPILE=~/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
